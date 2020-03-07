96c538f424831a398c7fda26655f636d62c2cdef
Dominic Fournier <dominicfournier@outlook.com>
2020-03-07 00:07:44 -0500

    - Updated email with domain adress.

--- components/nav.js
+++ components/nav.js
@@ -17,7 +17,7 @@ const Nav = () => {
     <div id="nav">
       <div>Dominic Fournier</div>
       <div>
-        <a href="mailto:dominicfournier@outlook.com"><i className="fi-xnsuxl-envelope-text-solid" /></a>
+        <a href="mailto:me@dominicfournier.com"><i className="fi-xnsuxl-envelope-text-solid" /></a>
         <a target="_blank" href={`/files/CV_Dominic_Fournier2020_${lang}.pdf`}><i className="fi-xnsuxl-file-solid" /></a>
         <a target="_blank" href="https://github.com/DominicF96"><i className="fi-xnsuxl-github" /></a>
         <a target="_blank" href="https://www.linkedin.com/in/dominic-fournier-1b068212a/"><i className="fi-cnsuxl-linkedin" /></a>
